abd22e61eb3a6bc500219136f321533239ab14a3
Jack <kbolot312@gmail.com>
2023-02-22 22:21:53 +0600

    direction changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Header from 'components/Header'
-import { Login } from 'components/Login'
+import Header from '../components/Header'
+import { Login } from '../components/Login'
 
 export default function Home() {
   return (
