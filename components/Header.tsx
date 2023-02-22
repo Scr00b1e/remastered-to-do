fd45af317e0d374f8636f96092af5c53da9778ce
Jack <kbolot312@gmail.com>
2023-02-22 22:18:10 +0600

    added few components

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='bg-black py-5'>
+            <h1 className='text-white'>Header</h1>
+        </header>
+    )
+}
+
+export default Header
