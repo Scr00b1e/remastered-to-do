6e46a31ec4843325a0c1e267a2d8e5f1bc6c257e
Jack <kbolot312@gmail.com>
2023-02-23 22:26:18 +0600

    beggining of the main logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,12 @@ import { Login } from '@/components/Login'
 
 export default function Home() {
   return (
-    <div className='w-full w-max-lg flex justify-center'>
-      <Login />
+    // <div className='w-full max-w-7xl flex justify-center'>
+    //   <Login />
+    // </div>
+    <div className='w-full max-w-7xl flex flex-col justify-center items-center'>
+      <p className='uppercase text-4xl text-center mb-10'>Text here something</p>
+      <input type="text" className='outline-none p-3 text-slate-800' />
     </div>
   )
 }
