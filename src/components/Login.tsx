e44a72765007e826eaaf9077af51c470fc74c3c6
Jack <kbolot312@gmail.com>
2023-02-23 21:39:29 +0600

    login almostly done

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
 
 export const Login = () => {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const [error, setError] = React.useState('')
+
+    const onSubmit = () => {
+        if (!email || !password) {
+            setError('Please enter email or password')
+            return
+        }
+    }
+
     return (
         <div className='flex-1 flex flex-col justify-center items-center bg-slate-800 gap-2 w-full max-w-lg px-2 py-10 rounded-3xl'>
             <h2 className='text-2xl font-bold'>LOGIN</h2>
-            <input type="text" className='outline-none text-slate-900 p-3 w-full max-w-xs' placeholder='Email Address...' />
-            <input type="text" className='outline-none text-slate-900 p-3 w-full max-w-xs' placeholder='Password...' />
+            {error && <div className='text-rose-400'>{error}</div>}
+            <input type="text" className='outline-none text-slate-900 p-3 w-full max-w-xs' placeholder='Email Address...'
+                value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" className='outline-none text-slate-900 p-3 w-full max-w-xs' placeholder='Password...'
+                value={password} onChange={(e) => setPassword(e.target.value)} />
             <button className='w-full max-w-xs border py-5 font-bold text-lg uppercase tracking-wider hover:bg-slate-700
-            duration-300'>Sumbit</button>
+            duration-300' onClick={onSubmit}>Sumbit</button>
         </div>
     )
 }
