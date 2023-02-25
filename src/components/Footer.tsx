ab87be65485e2444e466c6e6a654815dd4294fe3
Jack <kbolot312@gmail.com>
2023-02-25 21:01:19 +0600

    footer changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer>
-            footer
+        <footer className='flex justify-center items-center py-5 border-t-2'>
+            <a href="https://github.com/Scr00b1e" target={'_blank'}>
+                <img src="https://cdn-icons-png.flaticon.com/512/270/270798.png" alt="" className='w-10 h-10 cursor-pointer mx-1' />
+            </a>
+            <a href="https://twitter.com/scr00b1e" target={'_blank'}>
+                <img src="https://cdn-icons-png.flaticon.com/512/3670/3670127.png" alt="" className='w-10 h-10 cursor-pointer mx-1' />
+            </a>
         </footer>
     )
 }
