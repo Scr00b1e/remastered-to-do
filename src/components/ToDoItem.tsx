f7e848eec1e8cc6e62b443dc5edca116a8c9e7e7
Jack <kbolot312@gmail.com>
2023-02-26 18:21:59 +0600

    additional ui changes

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ToDoItem = () => {
     return (
-        <div className='w-full h-auto min-h-lg m-5 p-5 border-cyan-50 border-2 bg-slate-800'>
-            <p className=''>hey</p>
+        <div className='w-full h-auto min-h-lg m-5 p-5 border-cyan-50 border-2 bg-slate-800 rounded-3xl'>
+            <p className='text-3xl'>sup bros how ya doin sup bros how ya doin sup bros how ya doin sup bros how ya doin sup bros how ya doin sup bros how ya doin sup bros how ya doin sup bros how ya doin sup bros how ya doin sup bros how ya doin sup bros how ya doin sup bros how ya doin</p>
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,15 @@ import ToDoItem from '@/components/ToDoItem'
 
 export default function Home() {
   return (
+
     // <div className='w-full max-w-7xl flex justify-center'>
     //   <Login />
     // </div>
+
     <div className='w-full max-w-7xl flex flex-col justify-center items-center'>
       <p className='uppercase text-4xl text-center mb-10'>Text here something</p>
-      <input type="text" className='outline-none p-3 text-slate-800' />
+      <input type="text" className='outline-none p-3 text-slate-800 w-full max-w-3xl' />
+      <button className='mt-2 px-5 py-1 border-2 border-white bg-gray-600 text-xl font-bold'>Add To-Do</button>
       <div className='w-full flex justify-center items-center flex-col mt-5'>
         <ToDoItem />
       </div>
