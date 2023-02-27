45da2b1f6677e6682211287b16eb5053cc85e4a9
Jack <kbolot312@gmail.com>
2023-02-27 22:08:41 +0600

    ui changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,17 @@ export default function Home() {
     // </div>
 
     <div className='w-full max-w-7xl flex flex-col justify-center items-center'>
-      <p className='uppercase text-4xl text-center mb-10'>Text here something</p>
+      <p className='uppercase text-4xl text-center mb-10 mt-28'>Text here something</p>
       <input type="text" className='outline-none p-3 text-slate-800 w-full max-w-3xl' />
       <button className='mt-2 px-5 py-1 border-2 border-white bg-gray-600 text-xl font-bold'>Add To-Do</button>
       <div className='w-full flex justify-center items-center flex-col mt-5'>
+        <ToDoItem />
+        <ToDoItem />
+        <ToDoItem />
+        <ToDoItem />
+        <ToDoItem />
+        <ToDoItem />
+        <ToDoItem />
         <ToDoItem />
       </div>
     </div>
